a0e91de802322f99f9cab6fdd05593f6819952d5
MuxusTL <muxus.pte@gmail.com>
2026-09-26 15:47:52 +0700

    Fix Floating Menu, Theme

--- update_service.py
+++ update_service.py
@@ -1,4 +1,6 @@
-package dev.nitroplus.ui;
+import re
+
+content = """package dev.nitroplus.ui;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -211,3 +213,7 @@ public class FloatingMenuService extends Service {
         if (menuView != null) windowManager.removeView(menuView);
     }
 }
+"""
+
+with open("app/src/main/java/dev/nitroplus/ui/FloatingMenuService.java", "w") as f:
+    f.write(content)
